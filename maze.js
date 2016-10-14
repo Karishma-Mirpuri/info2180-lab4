b01a9492652f3ecc2332c35117eeea65f79439d6
Karishma-Mirpuri <mirpurikarishma@yahoo.com>
2016-10-14 18:28:14 -0500

    Exercise 3

--- maze.js
+++ maze.js
@@ -1,13 +1,27 @@
-//JavaScript Code for Exercise 2
-window.onload = function(){
-	var allboun = document.querySelectorAll("div#maze div.boundary");
+//JavaScript Code for Exercise 3
+var flag = false; //boolean to keep track if the user hit any maze walls.
+
+window.onload = function() {
+    var e = document.getElementById("end");
+    e.addEventListener("mouseover", gameOver);
+    var allboun = document.querySelectorAll("div#maze div.boundary");
 	for (var x = 0; x < allboun.length; x++){
-		allboun[x].addEventListener("mouseover", turnRed);
-	}
-}
+        allboun[x].addEventListener("mouseover", turnRed);
+    }
+};
+
 function turnRed() {
+	flag = true;
     var allboun = document.querySelectorAll("div#maze div.boundary");
     for (var x = 0; x < allboun.length; x++){
         allboun[x].setAttribute("class", "boundary youlose");
+    }
+}
+
+function gameOver() {
+    if(flag) {
+        alert("Oops! You lost.");
+    } else {
+        alert("Congratulations! You win! :)");
     }
 }
